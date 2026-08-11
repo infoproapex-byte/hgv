b02ec94f02964e61348ce4f6d8c841c68466ed4f
infoproapex-byte <infoproapex@gmail.com>
2026-08-11 19:15:15 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -65,5 +65,30 @@ self.addEventListener("fetch", function (event) {
       .catch(function () {
         return caches.match(req);
       })
+    self.addEventListener("notificationclick", function (event) {
+  event.notification.close();
+
+  event.waitUntil(
+    self.clients.matchAll({
+      type: "window",
+      includeUncontrolled: true
+    }).then(function (clientList) {
+
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+
+        if ("focus" in client) {
+          return client.focus();
+        }
+      }
+
+      if (self.clients.openWindow) {
+        return self.clients.openWindow(
+          "https://hgvtalk.live/wtd.html"
+        );
+      }
+    })
+  );
+});
   );
 });
